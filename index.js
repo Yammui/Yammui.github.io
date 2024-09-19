96e35b49ce09b0e9fda73500c5961d9257f76203
Yammui <eyam2007@outlook.com>
2024-09-19 20:54:44 +0000

    commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+//console.log("hello world");
+const audio = new Audio("s1.wav");
+const audio2 = new Audio("s2.mp3");
+const cat = document.querySelector(".booody");
+const cat2 = document.querySelector(".bigfatcat");
+
+cat.addEventListener("click", () => {
+  audio.play();
+});
+
+cat2.addEventListener("click", () => {
+  audio2.play();
+});
